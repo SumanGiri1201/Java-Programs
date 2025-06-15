379c4e6499df3b8d7302ffa4961236bff5016c02
SumanGiri1201 <Sumangiri2001@>
2025-06-15 20:08:49 +0530

    init commit

--- src/PalindromeNumberCheck.java
+++ src/PalindromeNumberCheck.java
@@ -1,5 +1,21 @@
 //check the given number is Palindrome or not
 
 public class PalindromeNumberCheck {
+public static void main(String[] args) {
+    int n = 121, pal, r, rev = 0;
+    pal = n;
 
+    while(n>0)
+    {
+        r = n%10;
+        rev = rev*10 + r;
+        n = n/10;
+    }
+    if(rev==pal) {
+        System.out.println("Number is palindrome " + pal);
+    }
+    else{
+        System.out.println("Number is not palindrome " + pal);
+    }
+}
 }
