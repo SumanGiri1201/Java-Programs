48dbe347768b67266351a8ed8727bd1d1c3a7758
SumanGiri1201 <Sumangiri2001@>
2025-06-15 20:52:24 +0530

    init commit

---
+++ src/PyramidPattern1.java
@@ -0,0 +1,11 @@
+public class PyramidPattern1 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<=n;i++){
+            System.out.print("\n");
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+        }
+    }
+}
