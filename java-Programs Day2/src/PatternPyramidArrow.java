4f60f3e27e42e3bfaa5e744d6855df9d078ac91d
SumanGiri1201 <suman.giri.1201@gmail.com>
2025-06-18 12:18:16 +0530

    init commit

---
+++ java-Programs Day2/src/PatternPyramidArrow.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PatternPyramidArrow {
+    public static void main(String[] args) {
+        int i, j, n;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value:");
+        n = in.nextInt();
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print(" " + j);
+            }
+            System.out.println();
+        }
+        for (i = n - 1; i >= 1; i--) {
+            for (j = 1; j <= i; j++) {
+                System.out.print(" " + j);
+            }
+            System.out.println();
+        }
+    }
+}
