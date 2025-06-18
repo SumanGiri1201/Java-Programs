ad4b95b5d674e1aaa1e33960a670b1bee9287a0d
SumanGiri1201 <suman.giri.1201@gmail.com>
2025-06-18 18:29:43 +0530

    init commit

---
+++ java-Programs Day2/src/Cube.java
@@ -0,0 +1,9 @@
+// Volume of a cube
+
+public class Cube {
+        public static void main(String arg[]) {
+            int side=5;
+            float volume=side * side * side;
+            System.out.println("Volume of Cube :"+ volume);
+        }
+    }
