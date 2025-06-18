99e2b748d4cb4a656e76a7e1a27121c9dca1b46f
SumanGiri1201 <suman.giri.1201@gmail.com>
2025-06-18 13:06:32 +0530

    init commit

---
+++ java-Programs Day2/src/Star3.java
@@ -0,0 +1,19 @@
+public class Star3 {
+    public static void main(String[] args) {
+        {
+            int i, j, k, n = 3;
+            for (i = 0; i < n; i++) {
+                for (j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+                for (j = (n - i); j >= 2; j--) {
+                    System.out.print(" ");
+                }
+                for (k = i; k >= 0; k--) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
