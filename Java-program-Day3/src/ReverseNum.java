17adc050744af832b4f59fb79d72bf8283206b9d
SumanGiri1201 <suman.giri.1201@gmail.com>
2025-06-19 15:39:02 +0530

    init commit

---
+++ Java-program-Day3/src/ReverseNum.java
@@ -0,0 +1,15 @@
+public class ReverseNum {
+    public static void main(String args[]) {
+        int rev = 0;
+        int num = 1234;
+        int n = num;
+        while (num > 0) {
+            int rem = num % 10;
+            rev = rem + (rev * 10);
+            num = num / 10;
+        }
+        System.out.println("Number = " + n);
+        System.out.println("Reverse = " + rev);
+    }
+}
+
